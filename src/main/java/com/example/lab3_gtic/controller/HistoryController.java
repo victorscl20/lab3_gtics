4543f18438f4a060145e2ae0b93aebf1538e9109
Ronny Pastor <a20185534@pucp.edu.pe>
2022-09-20 21:34:09 -0500

    rararar

--- src/main/java/com/example/lab3_gtic/controller/HistoryController.java
+++ src/main/java/com/example/lab3_gtic/controller/HistoryController.java
@@ -2,12 +2,14 @@ package com.example.lab3_gtic.controller;
 
 import com.example.lab3_gtic.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 @Controller
+@RequestMapping("/history")
 public class HistoryController {
+    @Autowired
+    EmployeeRepository employeeRepository;
 
 
 }
