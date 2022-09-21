0285a57f32f3168578168833be439abdda82ae7c
Victor Calderon <victor.calderon@pcup.edu.pe>
2022-09-20 20:37:44 -0500

    faa

---
+++ src/main/java/com/example/lab3_gtic/Lab3GticApplication.java
@@ -0,0 +1,13 @@
+package com.example.lab3_gtic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab3GticApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab3GticApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lab3_gtic/Lab3GticApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab3_gtic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab3GticApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
