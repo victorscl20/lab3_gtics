1da92a6d1c61f09df9b7af3eb41f187abca6d253
raykevinconcha <tudela2352>
2022-09-20 21:29:20 -0500

    history contro

--- src/main/java/com/example/lab3_gtic/controller/HistoryController.java
+++ src/main/java/com/example/lab3_gtic/controller/HistoryController.java
@@ -1,4 +1,18 @@
 package com.example.lab3_gtic.controller;
 
+import com.example.lab3_gtic.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
 public class HistoryController {
+    @Autowired
+    EmployeeRepository employeeRepository;
+    @GetMapping("/historial")
+    public String listaEmployee(Model model){
+        model.addAtribute("listaHistory",employeeRepository.);
+        return  "/historial/lista";
+    }
+
+
 }
