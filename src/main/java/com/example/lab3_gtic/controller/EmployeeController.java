3c849cbaa20b398de8e509d7f91b853d652a5a0c
Victor Calderon <victor.calderon@pcup.edu.pe>
2022-09-20 21:06:22 -0500

    controller employe

---
+++ src/main/java/com/example/lab3_gtic/controller/EmployeeController.java
@@ -0,0 +1,24 @@
+package com.example.lab3_gtic.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/employee")
+public class EmployeeController {
+
+    @Autowired
+    EmployeesRepository employeesRepository;
+
+
+    @GetMapping(value = {"","/list"})
+    public String listaEmployee(Model model){
+        model.addAttribute("listaEmployee", employeesRepository.findAll());
+        model.addAttribute("listaJobs", jobsRepository.findAll());
+        model.addAttribute("listaDepartments", departmentsRepository.findAll());
+        return "employee/lista";
+    }
+}
