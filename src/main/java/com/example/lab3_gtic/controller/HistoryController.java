75e7bd9aec988df4997924e1515b08636351b1ac
raykevinconcha <tudela2352>
2022-09-20 21:31:53 -0500

    history control

--- src/main/java/com/example/lab3_gtic/controller/HistoryController.java
+++ src/main/java/com/example/lab3_gtic/controller/HistoryController.java
@@ -2,9 +2,11 @@ package com.example.lab3_gtic.controller;
 
 import com.example.lab3_gtic.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
+@Controller
 public class HistoryController {
     @Autowired
     EmployeeRepository employeeRepository;
